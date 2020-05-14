68390827e53b0b47a8023ef2998ec628b60d1980
Antti Saaristo <antti.saaristo@gmail.com>
2020-05-14 23:33:32 +0300

    invalid login message

--- testpages/js/login.js
+++ testpages/js/login.js
@@ -1,18 +1,23 @@
-var attempt = 3; // Variable to count number of attempts.
+var attempt = 3;    // variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if ( username == "user" && password == "password"){
-    alert ("Login succeeded for " + username );
-    window.location = "success.html"; // Redirecting to other page.
-    return false;
+        alert ("Login succeeded for " + username );
+        window.location = "success.html";   // redirecting to other page.
+        return false;
 }
 else { 
-    attempt --; // Decrementing by one.
-    alert("You have left "+attempt+" attempt;");
-    // Disabling fields after 3 attempts.
-    if( attempt == 0){
+    attempt --;     // decrementing by one.
+    // alert("You have left "+attempt+" attempt;");
+    document.getElementById("error").innerHTML = ""
+    if (attempt != 0) {
+        document.getElementById("error").innerHTML = "Incorrect username or password.<br>You have  " + attempt + " attempts left"
+    }
+
+    // disabling fields after 3 attempts.
+    if( attempt == 0) {
         document.getElementById("username").disabled = true;
         document.getElementById("password").disabled = true;
         document.getElementById("submit").disabled = true;
